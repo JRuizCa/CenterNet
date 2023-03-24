e13e8c0823225a28c221d60d4a5f66e84d194407
JRuizCa <julia.ruizcardon@gmail.com>
2023-03-24 16:37:27 +0100

    Changes to run implementation

--- src/lib/models/model.py
+++ src/lib/models/model.py
@@ -9,9 +9,12 @@ import os
 
 from .networks.msra_resnet import get_pose_net
 from .networks.dlav0 import get_pose_net as get_dlav0
-from .networks.pose_dla_dcn import get_pose_net as get_dla_dcn
-from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
+#from .networks.pose_dla_dcn import get_pose_net as get_dla_dcn
+#from .networks.resnet_dcn import get_pose_net as get_pose_net_dcn
 from .networks.large_hourglass import get_large_hourglass_net
+
+get_dla_dcn = []
+get_pose_net_dcn = []
 
 _model_factory = {
   'res': get_pose_net, # default Resnet with deconv
