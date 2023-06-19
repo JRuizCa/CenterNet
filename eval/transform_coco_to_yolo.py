e3fae04dacb992d63b285d5fe3b44f3f51dd2951
JRuizCa <julia.ruizcardon@gmail.com>
2023-06-19 22:30:13 +0200

    transform predictions and gt into Yolo for evaluation of the model

---
+++ eval/transform_coco_to_yolo.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+import pandas as pd
+
+
+def transform_preds_results_json_to_txt(results_json):
+    with open(results_json) as json_file:
+        data = json.load(json_file)
+    data = pd.DataFrame(data)
+    data['bbox'] = data['bbox'].apply(lambda x: [x[0], x[1], np.round(x[0]+x[2], 3), np.round(x[1]+x[3], 3)])
+    data['category_id'] = data['category_id'].apply(lambda x: 'barcode')
+    data.set_index('image_id', inplace=True)
+    for index in data.index.unique():
+        with open(f'eval/input/detection-results/{index}.txt', 'w') as txt_file:
+            for i in range(len(data[data.index == index])):
+                data_index = data[data.index == index]
+                txt_file.write(f"barcode ")
+                txt_file.write(f"{data_index['score'].iloc[i]} ")
+                txt_file.write(f"{data_index['bbox'].iloc[i][0]} {data_index['bbox'].iloc[i][1]} {data_index['bbox'].iloc[i][2]} {data_index['bbox'].iloc[i][3]} ")
+                txt_file.write("\n")
+    txt_file.close()
+
+def transform_gt_json_to_txt(gt_json):
+    with open(gt_json) as json_file:
+        data = json.load(json_file)
+    data = pd.DataFrame(data['annotations'])
+    data['bbox'] = data['bbox'].apply(lambda x: [x[0], x[1], x[0]+x[2], x[1]+x[3]])
+    data['category_id'] = data['category_id'].apply(lambda x: 'barcode')
+    data.set_index('image_id', inplace=True)
+    for index in data.index.unique():
+        with open(f'eval/input/ground-truth/{index}.txt', 'w') as txt_file:
+            for i in range(len(data[data.index == index])):
+                data_index = data[data.index == index]
+                txt_file.write(f"barcode ")
+                txt_file.write(f"{data_index['bbox'].iloc[i][0]} {data_index['bbox'].iloc[i][1]} {data_index['bbox'].iloc[i][2]} {data_index['bbox'].iloc[i][3]} ")
+                txt_file.write("\n")
+        txt_file.close()
+
+
+def main():
+    transform_preds_results_json_to_txt('eval/input/detection-results/results.json')
+    transform_gt_json_to_txt('eval/input/ground-truth/instances_test.json')
+        
+if __name__ == "__main__":
+    main()
